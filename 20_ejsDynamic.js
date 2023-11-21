f463974394593aeae2bb3299aa3de50e645eb711
ankit1519 <raazankit1502@gmail.com>
2023-11-22 03:45:15 +0530

    loop in ejs

---
+++ 20_ejsDynamic.js
@@ -0,0 +1,32 @@
+const express=require('express')
+const path =require('path')
+
+const app=express()
+const publicPath=path.join(__dirname,'public_17')
+//console.log(publicPath);
+app.set('view engine','ejs')
+app.get("/",(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+app.get("/about",(req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+app.get("/blog",(req,res)=>{
+    res.sendFile(`${publicPath}/blog.html`)
+})
+//using ejs
+app.get("/profile",(req,res)=>{
+    const data={
+        name:"Ankit Raj",
+        Reg:99,
+        mail:'raazankit1502@gmail.com',
+        skills:['react.js','js','css','tailwind','typescript']
+    }
+    res.render('profile',{data})
+})
+
+app.get("*",(req,res)=>{
+    res.sendFile(`${publicPath}/noPage.html`)
+})
+//app.use(express.static(publicPath))
+app.listen(5000);
